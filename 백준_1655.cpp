fbfa8bbb352e61f1dbc0dc3e3908bc8fe22f7f0c
rinrin529 <45943080+rinrin529@users.noreply.github.com>
2021-01-20 13:46:18 +0900

    min, max heap

--- 백준_1655.cpp
+++ 백준_1655.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <queue>
+#include <stdio.h>
 
 using namespace std;
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	priority_queue< int, vector<int>, greater<int> > pq;
+	priority_queue<int, vector<int>, less<int> > maxHeap;
+	priority_queue<int, vector<int>, greater<int> > minHeap;
 	int arr[100001] = { 0, };
 	int n = 0;
 
@@ -16,14 +18,27 @@ int main(void) {
 	}
 
 	for (int i = 0; i < n; i++) {
-		if ((i+1) % 2 == 1 && (i+1) != 1) { //홀수개일때
-			pq.push(arr[i]);
-			pq.pop();
+		if (maxHeap.empty()) {
+			maxHeap.push(arr[i]);
+		}
+		else if (maxHeap.size() == minHeap.size()) {
+			maxHeap.push(arr[i]);
 		}
 		else {
-			pq.push(arr[i]);
+			minHeap.push(arr[i]);
 		}
-		cout << pq.top() << endl;
+
+		if (!(maxHeap.empty())&&!(minHeap.empty()) &&maxHeap.top() >= minHeap.top()) {
+			int a = maxHeap.top();
+			int b = minHeap.top();
+
+			maxHeap.pop();
+			minHeap.pop();
+			maxHeap.push(b);
+			minHeap.push(a);
+		}
+		printf("%d\n", maxHeap.top());
 	}
+	
 	return 0;
 }
